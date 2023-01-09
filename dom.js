6f21f9000dc88bdb0581f807c802e69665acf53b
hammad <65504494+hammadharoonk@users.noreply.github.com>
2023-01-09 17:55:59 +0300

    doms new

--- dom.js
+++ dom.js
@@ -1,1 +1,23 @@
-let chat = 'shit'
+const title = document.getElementById("page-title");
+
+title.classList.add("purple");
+const thing = document.querySelector("h1");
+
+
+// for (let title of Array.from(document.getElementsByTagName("h1"))) {
+//     console.log(title)
+//     title.style.backgroundColor = "red"
+// }
+
+// console.log(document.getElementsByClassName("purple"))
+// for (let item of Array.from(document.querySelectorAll)) {
+//     console.log(item)
+// }
+
+const cards = document.getElementsByClassName("card");
+
+for (let card of Array.from(cards)) {
+    const children = card.children;
+    const text = children[1];
+    text.textContent = "whatever"
+}
