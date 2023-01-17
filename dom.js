d38ea54d9193e8d371d7c982ba518101c47ac733
hammadharoonk <hammadharoon93@gmail.com>
2023-01-17 23:38:02 +0300

    DOM

--- dom.js
+++ dom.js
@@ -1,8 +1,7 @@
 const title = document.getElementById("page-title");
 
-title.classList.add("purple");
+// title.classList.add("purple");
 const thing = document.querySelector("h1");
-
 
 // for (let title of Array.from(document.getElementsByTagName("h1"))) {
 //     console.log(title)
@@ -16,8 +15,42 @@ const thing = document.querySelector("h1");
 
 const cards = document.getElementsByClassName("card");
 
-for (let card of Array.from(cards)) {
-    const children = card.children;
-    const text = children[1];
-    text.textContent = "whatever"
+// for (let card of Array.from(cards)) {
+//     const children = card.children;
+//     const text = children[0];
+//     text.textContent = "whatever";
+//     // card.parentElement.classList.add("purple")
+// }
+
+const todoList = [
+  "finish research article",
+  "make revit plugin",
+  "create browser based ifc game",
+];
+
+const container = document.querySelector(".mainContainer");
+
+const buttonsArray = [];
+
+for (let task of todoList) {
+  const text = document.createElement("p");
+  
+
+  text.addEventListener("mouseenter", (event) => {
+    text.classList.add("purple")
+  })
+
+  text.addEventListener("mouseleave", (event) => {
+    text.classList.remove("purple")
+  })
+
+  text.addEventListener("mousedown", (event) => {
+    buttonsArray.forEach(button => button.classList.remove("selected"))
+    console.log(`selected task ${task}`)
+    text.classList.add("selected")
+    buttonsArray.push(text)
+  })
+
+  text.textContent = task;
+  container.appendChild(text);
 }
